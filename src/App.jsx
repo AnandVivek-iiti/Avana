e348502bcf2cf934a826c7f5088cb2821fc2c603
Anandvivek-iiti <anand1234.vivek@gmail.com>
2026-06-17 16:04:57 +0530

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from './context/ThemeContext';
+import { ThemeProvider } from './context/Themecontext';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
